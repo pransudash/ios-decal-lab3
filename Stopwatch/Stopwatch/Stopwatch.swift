4619f49e92972b01c5b25be952b9332576b4c1eb
Pransu Dash <pdash@Pransus-MacBook-Pro.local>
2017-02-21 18:37:54 -0800

    done with functionality

--- Stopwatch/Stopwatch/Stopwatch.swift
+++ Stopwatch/Stopwatch/Stopwatch.swift
@@ -10,5 +10,50 @@ import Foundation
 
 class Stopwatch {
     
+    private var ms: Int
+    
+    private var sec: Int
+    
+    private var min: Int
+    
+    private var startTime: Date
+    
+    init() {
+        self.min = 0
+        self.sec = 0
+        self.ms = 0
+        self.startTime = Date()
+    }
+    
+    func getMin() -> Int {
+        if(self.sec >= 59) {
+            self.min += 1
+            self.sec = 0;
+        }
+        
+        var m: TimeInterval = self.startTime.timeIntervalSince(self.startTime)
+        
+        return self.min
+    }
+    
+    func getSec() -> Int {
+        if(self.ms >= 99) {
+            self.sec += 1
+            self.ms = 0;
+        }
+        return self.sec
+    }
+    
+    func getMs() -> Int {
+        self.ms += 1
+        return self.ms
+    }
+    
+    func reset() {
+        self.min = 0
+        self.sec = 0
+        self.ms = 0
+    }
+    
 }
 
--- Stopwatch/Stopwatch/ViewController.swift
+++ Stopwatch/Stopwatch/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeDisplay: UILabel!
+    
+    let stopwatch = Stopwatch()
+    
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 0.005, target: self,
+                             selector: #selector(ViewController.updateTime),
+                             userInfo: nil,
+                             repeats: true)
+    }
 
+    @IBAction func stopPressed(_ sender: Any) {
+        timer?.invalidate()
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        stopwatch.reset()
+        updateView(min: 0, sec: 0, ms: 0);
+    }
+    
+    func updateTime() {
+        let min: Int = stopwatch.getMin()
+        let sec: Int = stopwatch.getSec()
+        let ms: Int = stopwatch.getMs()
+        
+        updateView(min: min, sec: sec, ms: ms);
+    }
+    
+    func updateView(min: Int, sec: Int, ms: Int) {
+        let timeString = String(format: "%02d:%02d:%02d", min, sec, ms)
+        timeDisplay.text = timeString;
+    }
 }
 
